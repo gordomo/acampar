97ba57b5dd693e76344f2595725ca30f07500130
mmori <martin.mori@martindale.com>
2016-05-05 13:34:33 -0300

    modificaciones - modificaciones en db

--- categorias.sql
+++ categorias.sql
@@ -44,12 +44,6 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 -- Volcado de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`id`, `nombre`, `foto`, `descripcion_corta`, `descripcion`, `id_tour`, `cat_superior`, `lat`, `long`, `polylines`) VALUES
-(26, 'Opción 1123123123', 'img/categorias/7ebf0077bf_pink-floyd-11.jpg', '', '', 1, 0, '-32.6517842', '-70.2910596', ''),
-(27, 'Martin', 'img/categorias/071568a11a_wdf_1382273.jpg', 'corta3', '“Gastronomía y Alta Cocina” brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía. Además obtendrás las herramientas para emprender un negocio culinario y alcanzar así la cima del éxito, haciendo de la cocina tu profesión. Ver mas...', 2, 5, '-32.948920', '-60.646229', ''),
-(28, 'Hijo aconca', 'img/categorias/a3f57962ae_pink-floyd-11.jpg', 'corta2', '“Gastronomía y Alta Cocina” brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía. Además obtendrás las herramientas para emprender un negocio culinario y alcanzar así la cima del éxito, haciendo de la cocina tu profesión. Ver mas...', 2, 6, '-32.652922', '-70.011727', ''),
-(29, 'nuevo', 'img/categorias/fcda90a81d_hqdefault.jpg', 'corta2', '“Gastronomía y Alta Cocina” brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía. Además obtendrás las herramientas para emprender un negocio culinario y alcanzar así la cima del éxito, haciendo de la cocina tu profesión. Ver mas...', 2, 1, '-32.949568', '-60.648246', '');
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
